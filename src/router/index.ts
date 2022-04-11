b9c4b384b28f21e6668328bde1216c28228594cf
esan.huang <esan.huang@yesee.com.tw>
2022-04-11 11:46:03 +0800

    更改router設定

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 const routes = [
   {
@@ -38,6 +38,6 @@ const routes = [
   },
 ];
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
